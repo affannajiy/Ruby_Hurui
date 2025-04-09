a93e60204b03789f21ca2df0457da8d14cf0180c
Affan Najiy <affannajiy@gmail.com>
2025-04-09 23:21:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ruby_Hurui
+ Codes for Ruby
---
+++ testing.rb
@@ -0,0 +1,10 @@
+#Introduction to Ruby
+
+puts "Hello world"
+puts "Enter your name"
+name = gets.chomp #chomp removes the new line
+
+puts "Enter your age"
+age = gets.chomp
+
+puts "Your name is #{name} and your age is #{age}"
